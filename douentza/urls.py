f07e2ef4ff1f050c10c8dbdd7443d99d1ae5823e
renaud gaudin <rgaudin@gmail.com>
2013-08-29 18:41:55 +0000

    example generic form

--- douentza/urls.py
+++ douentza/urls.py
@@ -15,6 +15,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    url(r'^test$', 'douentza.views.form_test.tester', name='tester'),
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),

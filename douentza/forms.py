47dd86d0f68d32743b74fed9b7df0307485eb0b8
renaud gaudin <rgaudin@gmail.com>
2013-09-05 11:15:34 +0000

    fixed choicefield bug

--- douentza/forms.py
+++ douentza/forms.py
@@ -10,63 +10,38 @@ from django import forms
 from douentza.models import HotlineRequest, Project, Ethnicity, Entity
 from douentza.utils import EMPTY_ENTITY
 
-attrs = {'class': 'form-control'}
-
-wTextInput = forms.TextInput(attrs=attrs)
-wPasswordInput = forms.PasswordInput(attrs=attrs)
-wHiddenInput = forms.HiddenInput(attrs=attrs)
-wDateInput = forms.DateInput(attrs=attrs)
-wDateTimeInput = forms.DateTimeInput(attrs=attrs)
-wTimeInput = forms.TimeInput(attrs=attrs)
-wTextarea = forms.Textarea(attrs=attrs)
-wCheckboxInput = forms.CheckboxInput(attrs=attrs)
-wSelect = forms.Select(attrs=attrs)
-wNullBooleanSelect = forms.NullBooleanSelect(attrs=attrs)
-wSelectMultiple = forms.SelectMultiple(attrs=attrs)
-wRadioSelect = forms.RadioSelect(attrs=attrs)
-wCheckboxSelectMultiple = forms.CheckboxSelectMultiple(attrs=attrs)
-wFileInput = forms.FileInput(attrs=attrs)
-wClearableFileInput = forms.ClearableFileInput(attrs=attrs)
-wMultipleHiddenInput = forms.MultipleHiddenInput(attrs=attrs)
-wSplitDateTimeWidget = forms.SplitDateTimeWidget(attrs=attrs)
-
 
 class BasicInformationForm(forms.Form):
 
-    request_id = forms.IntegerField(widget=wHiddenInput)
+    request_id = forms.IntegerField(widget=forms.HiddenInput)
     responded_on = forms.DateTimeField(label="Date de l'appel",
                                        help_text="Format: JJ/MM/AAAA",
-                                       widget=wSplitDateTimeWidget)
+                                       widget=forms.SplitDateTimeWidget)
 
-    age = forms.IntegerField(label="Age", required=False, widget=wTextInput)
+    age = forms.IntegerField(label="Age", required=False, widget=forms.TextInput)
     sex = forms.ChoiceField(label="Sexe", required=False,
                             choices=HotlineRequest.SEXES.items(),
-                            widget=wSelect)
-    duration = forms.IntegerField(label="Durée", widget=wTextInput)
-    ethnicity = forms.ChoiceField(label="Ethnie", required=False, choices=[], widget=wSelect)
-    project = forms.ChoiceField(label="Projet", required=False, choices=[], widget=wSelect)
+                            widget=forms.Select)
+    duration = forms.IntegerField(label="Durée", widget=forms.TextInput)
+    ethnicity = forms.ChoiceField(label="Ethnie", required=False, widget=forms.Select)
+    project = forms.ChoiceField(label="Projet", required=False, widget=forms.Select)
 
-    region = forms.ChoiceField(label="Région", choices=[], widget=wSelect)
-    cercle = forms.CharField(label="Cercle", widget=wSelect, required=False)
-    commune = forms.CharField(label="Commune", widget=wSelect, required=False)
-    village = forms.CharField(label="Village", widget=wSelect, required=False)
+    region = forms.ChoiceField(label="Région", choices=[], widget=forms.Select)
+    cercle = forms.CharField(label="Cercle", widget=forms.Select, required=False)
+    commune = forms.CharField(label="Commune", widget=forms.Select, required=False)
+    village = forms.CharField(label="Village", widget=forms.Select, required=False)
 
     def __init__(self, *args, **kwargs):
         super(BasicInformationForm, self).__init__(*args, **kwargs)
-        all_ethnicity = [(e.slug, e.name) for e in Ethnicity.objects.order_by('name')]
+
+        all_ethinicty = [(e.slug, e.name) for e in Ethnicity.objects.order_by('name')]
         all_project = [(p.id, p.name) for p in Project.objects.order_by('name')]
         all_region = [(EMPTY_ENTITY, "INCONNUE")] + [(e.slug, e.name)
                       for e in Entity.objects.filter(entity_type=Entity.TYPE_REGION)]
 
-        self.fields['ethnicity'] = forms.ChoiceField(required=False,
-                                                     choices=all_ethnicity,
-                                                     widget=wSelect)
-        self.fields['project'] = forms.ChoiceField(required=False,
-                                                   choices=all_project,
-                                                   widget=wSelect)
-        self.fields['region'] = forms.ChoiceField(required=False,
-                                                  choices=all_region,
-                                                  widget=wSelect)
+        self.fields['ethnicity'].choices = all_ethinicty
+        self.fields['project'].choices = all_project
+        self.fields['region'].choices = all_region
 
     def clean_village(self):
             ''' Returns a Village Entity from the multiple selects '''

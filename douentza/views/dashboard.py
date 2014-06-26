862605c8d712cb70c03b2acfa03f2345a5af366f
renaud gaudin <rgaudin@gmail.com>
2014-06-26 14:41:56 +0000

    fixed ping if no cluster for user

--- douentza/views/dashboard.py
+++ douentza/views/dashboard.py
@@ -95,7 +95,7 @@ def ping_html(request):
     if nb_events:
         html = render_to_string('dashboard_table.html',
                                 {'all_events': all_events(request.user),
-                                 'clusters': Cluster.objects.exclude(slug=request.user.cluster.slug).all()})
+                                 'clusters': Cluster.objects.exclude(slug=getattr(request.user.cluster, 'slug')).all()})
     else:
         html = None
 
